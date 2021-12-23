506303b2f20f4369a87448536189cfa3f3322502
Luis Caumel <luiscaumel@gmail.com>
2021-12-23 17:06:25 +0100

    [Update] add https certificate and remove nginx

--- web/videogame_platform/settings.py
+++ web/videogame_platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@@hbkidt@mcg5474k%bzm-9@411z^xhw5q#tb39z8xp6ny6l(4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","0.0.0.0"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
