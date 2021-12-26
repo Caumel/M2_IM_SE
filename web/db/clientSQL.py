dbe881ebcd3db18d102d0111ffbc2eb6f9f5b1b8
Luis Caumel <luiscaumel@gmail.com>
2021-12-26 02:45:57 +0100

    [Update] add mongo requests file

--- web/db/clientSQL.py
+++ web/db/clientSQL.py
@@ -18,4 +18,146 @@ mydb = mysql.connector.connect(
 
 client = mydb.cursor()
 
-client.execute("SELECT * FROM User")
+def getColumnNames(client,tableName):
+    columns = "*"
+    client.execute(f"SELECT {columns} FROM {tableName}")
+    columnNames = client.description
+    result = client.fetchall()
+    columnNames = [column[0] for column in columnNames]
+    return columnNames 
+
+def select(client,columns,table):
+    client.execute(f"SELECT {columns} FROM {table}")
+    result = client.fetchall()
+    result = [list(row) for row in result]
+    return result
+
+def selectWhere(client,columns, table, where):
+    client.execute(f"SELECT {columns} FROM {table} WHERE {where}")
+    result = client.fetchall()
+    result = [list(row) for row in result]
+    return result
+
+def selectOrder(client, columns, table, order):
+    client.execute(f"SELECT {columns} FROM {table} ORDER BY {order}")
+    result = client.fetchall()
+    result = [list(row) for row in result]
+    return result
+
+def selectOrderDesc(client, columns, table, order):
+    client.execute(f"SELECT {columns} FROM {table} ORDER BY {order} DESC")
+    result = client.fetchall()
+    result = [list(row) for row in result]
+    return result
+
+def selectWhereOrder(client,columns, table, where, order):
+    client.execute(f"SELECT {columns} FROM {table} WHERE {where} ORDER BY {order}")
+    result = client.fetchall()
+    result = [list(row) for row in result]
+    return result
+
+def listToDict(columnName, data):
+    listMongo = []
+    for row in data:
+        dictRow = {}
+        for index, column in enumerate(columnName):
+            dictRow[column] = row[index]
+        listMongo.append(dictRow)
+    return listMongo
+
+
+columns = "*"
+table = "Company"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+company = listToDict(columnName, data)
+
+columns = "*"
+table = "Genre"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+genre = listToDict(columnName, data)
+
+columns = "*"
+table = "Original_Game"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+original_game = listToDict(columnName, data)
+
+columns = "*"
+table = "Original_Game_Have_Genre"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+original_game_have_genre = listToDict(columnName, data)
+
+columns = "*"
+table = "User"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+user = listToDict(columnName, data)
+
+columns = "*"
+table = "Library"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+library = listToDict(columnName, data)
+
+columns = "*"
+table = "Game"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+game = listToDict(columnName, data)
+
+columns = "*"
+table = "User_Buy_Original_Game"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+user_buy_original_game = listToDict(columnName, data)
+
+columns = "*"
+table = "Community"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+community = listToDict(columnName, data)
+
+columns = "*"
+table = "User_Rate_Original_Game"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+user_rate_original_game = listToDict(columnName, data)
+
+columns = "*"
+table = "User_Follow_User"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+user_follow_user = listToDict(columnName, data)
+
+columns = "*"
+table = "User_Rate_Review"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+user_rate_review = listToDict(columnName, data)
+
+columns = "*"
+table = "Review"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+review = listToDict(columnName, data)
+
+columns = "*"
+table = "User_Subscribe_Community"
+columnName = getColumnNames(client,table)
+data = select(client,columns,table)
+user_subscribe_community = listToDict(columnName, data)
+
+
+##Company
+
+lista = [company]
+lista = [original_game,original_game_have_genre,genre]
+lista = [genre]
+lista = [user,community,user_subscribe_community]
+lista = [library,user]
+lista = [community,user]
+lista = [game,library,original_game]
+lista = [review,original_game,company,user]
